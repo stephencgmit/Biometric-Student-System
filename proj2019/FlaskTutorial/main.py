1bc9e09dbcd2c518c20ebf0694f78de5674ca3e8
stephencgmit <37638020+stephencgmit@users.noreply.github.com>
2019-02-15 20:03:29 +0000

    Add files via upload

--- proj2019/FlaskTutorial/main.py
+++ proj2019/FlaskTutorial/main.py
@@ -2,29 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home(user=None):
+    return render_template("homepage.html", user=user)
 
-@app.route("/login/<name>")
-def profile(name):
-    return render_template("login.html",name=name)
-
-
-# @app.route("/bacon", methods=['GET', 'POST'])
-# def bacon():
-#    if request.method == 'GET':
-#        return "You are using POST"
-#    else:
-#        return "You are probably using GET"
-# @app.route("/")
-# @app.route("/<user>")
-# def index(user=None):
-#     return render_template("user.hmtl", user=user)
 
 @app.route("/")
-@app.route("/<user>")
+@app.route("/login")
+def login(user=None):
+    return render_template("login.html", user=user)
+
+
+@app.route("/")
+@app.route("/homepage")
+def homepage(user=None):
+    return render_template("homepage.html", user=user)
+
+
+@app.route("/")
+@app.route("/register")
 def index(user=None):
-    return render_template("user.html", user=user)
+    return render_template("register.html", user=user)
+
+
+# @app.route("/")
+# @app.route("/login")
+# def index(user=None):
+#     return render_template("login.html", user=user)
 
 
 if __name__ == "__main__":
-
     app.run()
